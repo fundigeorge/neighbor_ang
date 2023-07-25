24926969e1cfe38c6f92a0982d2eb1f8e7649def
fundi <fundigeorge@gmail.com>
2023-07-25 22:40:45 +0300

    added best intersection function

--- geodesic.py
+++ geodesic.py
@@ -1,8 +1,8 @@
 # Python 3 program to calculate Distance Between Two Points on Earth
 from math import radians, cos, sin, asin, acos, sqrt, atan2, atan, pi
 import numpy as np
-
-class geodesic:
+import pandas as pd
+class Geodesic:
     """ a class for calculating geodesic measurements, distance between two points on the earth 
     surface, bearing between points, intersection between two lines from two points
 
@@ -21,15 +21,12 @@ class geodesic:
     bearing(lat1, lon1, lat2, lon2): calculate bearing between two point
     intersection(lat1, lon1, lat2, lon2, crs13, crs23): calculate intersection point of two lines
     """
-
-    #earth radius constant in kms
-    EARTH_RADIUS = 6371
-    lead = 0
-    def __init__(self, lat, lon, azimuth) -> None:
+   
+    def __init__(self, lat, lon) -> None:
         self.lat = lat
-        self.lon = lon
-        self.azimuth = azimuth  
-        
+        self.lon = lon 
+        self.half_bw = 30 
+        self.earth_circum = 40035      
         
     def distance(self,lat, lon):        
         """calculate the distance between point 1(lat1, lon1) and point 2(lat2, lon2)
@@ -63,19 +60,13 @@ class geodesic:
         input: Point A coord(lat1, lon1) and point B(lat2, lon2)
         output: bearing degrees
         """
-        #method 1
-        # y = sin(lon2-lon1) * cos(lat2)
-        # x = cos(lat1)*sin(lat2) - sin(lat1)*cos(lat2)*cos(lon2-lon1)
-        # z = atan2(y,x)
-        # brng = (z*180/pi+360) % (360) # in degrees, mod of 360
 
-        #method2
         #calculate distance between point 1 and point 2
         d=2*asin(sqrt((sin((self.lat-lat)/2))**2 +  cos(self.lat)*cos(lat)*(sin((self.lon-lon)/2))**2))
 
         #calculate the bearing based on position
         if sin(lon-self.lon)>0:      
-            tc1=acos((sin(lat2)-sin(self.lat)*cos(d))/(sin(d)*cos(self.lat)))    
+            tc1=acos((sin(lat)-sin(self.lat)*cos(d))/(sin(d)*cos(self.lat)))    
         else:
             tc1=2*pi-acos((sin(lat)-sin(self.lat)*cos(d))/(sin(d)*cos(self.lat)))    
 
@@ -83,7 +74,7 @@ class geodesic:
         tc1 = tc1*180/pi
         return tc1
 
-    def intersection(self, lat, lon, crs23):
+    def intersection(self, lat, lon, crs13, crs23):
         '''computing the intersection(point 3) of a line at azimuth 1 from point 1 and a line at 
         azimuth 2 from point 2
 
@@ -98,24 +89,19 @@ class geodesic:
         Returns:
             float: intersection distance from point 1 to point 3    
         '''
-
-
         #distance between point 1 and point 2
         dst12 = 2*asin(sqrt((sin((self.lat-lat)/2))**2 + cos(self.lat)*cos(lat)*sin((self.lon-lon)/2)**2))
         #calculating bearing from point 1 to 2 csr12 and from point 2 to 1 crs21
         if sin(lon-self.lon) > 0:
             crs12=acos((sin(lat)-sin(self.lat)*cos(dst12))/(sin(dst12)*cos(self.lat)))
             crs21=2.*pi-acos((sin(self.lat)-sin(lat)*cos(dst12))/(sin(dst12)*cos(lat)))
-            print(f'crs12 = {crs12} angle {crs12*180/pi} and crs21={crs21} and angle {crs21*180/pi}')
         else:
             crs12=2.*pi-acos((sin(lat)-sin(self.lat)*cos(dst12))/(sin(dst12)*cos(self.lat)))
             crs21=acos((sin(self.lat)-sin(lat)*cos(dst12))/(sin(dst12)*cos(lat)))
-            print(f'crs12 = {crs12} and crs21={crs21}')
 
         #calculate angles in the triangle(P1, P2, P3), ang1=(P2,P1,P3), ang2=(P1,P2,P3), 
-        ang1 = (self.azimuth - crs12 + pi)%(2*pi) - pi
+        ang1 = (crs13 - crs12 + pi)%(2*pi) - pi
         ang2 =  (crs21-crs23 + pi)%(2*pi) - pi
-        print(f'ang1={ang1*180/pi} and ang2={ang2*180/pi}')
 
         #check for parallel lines and ambiguous condition(i.e antipodal point)
         if (sin(ang1)==0 and sin(ang2)==0):
@@ -133,8 +119,8 @@ class geodesic:
             ang2=abs(ang2)
             ang3=acos(-cos(ang1)*cos(ang2)+sin(ang1)*sin(ang2)*cos(dst12)) 
             dst13=atan2(sin(dst12)*sin(ang1)*sin(ang2),cos(ang2)+cos(ang1)*cos(ang3))
-            lat3=asin(sin(self.lat)*cos(dst13)+cos(self.lat)*sin(dst13)*cos(self.azimuth))
-            dlon=atan2(sin(self.azimuth)*sin(dst13)*cos(self.lat),cos(dst13)-sin(self.lat)*sin(lat3))
+            lat3=asin(sin(self.lat)*cos(dst13)+cos(self.lat)*sin(dst13)*cos(crs13))
+            dlon=atan2(sin(crs13)*sin(dst13)*cos(self.lat),cos(dst13)-sin(self.lat)*sin(lat3))
             lon3=(self.lon+dlon+pi)%(2*pi) - pi
             
             # 6371 Radius of earth in kilometers to convert angular unit to distance unit. Use 3956 for miles, 
@@ -142,58 +128,82 @@ class geodesic:
             dst12 = dst12*6371
             lat3 = (lat3*180/pi) # in decimal degrees,
             lon3 = (lon3*180/pi) # in decimal degrees,
-            return dst12, dst13, lat3, lon3
-    
+            return dst13, lat3, lon3
 
-#test data
-#lat lon in radians
-lat1=radians(-1.2814722) #0.592539 -1.2814722 36.8209444
-lon1=radians(36.8209444) #2.066470
-#lat lon in radians
-lat2=radians(-1.280823) #0.709186 -1.280823 36.822151
-lon2=radians(36.822151) #1.287762
-#confirm bearing and distance
-source = geodesic(lat1, lon1, radians(50) )
-print("distance to point 2", source.distance(lat2, lon2))
-try:
-    print(source.bearing(lat2, lon2))
-    print(source.intersection(lat2, lon2, radians(270)))
-except ZeroDivisionError as error:
-    print("same location")
 
+    def best_intersection(self, lat2, lon2, crs13, crs23):
+        #calculate the beamwidth edges for source and target cell 
+        pos_source_crs13 = radians((crs13 + self.half_bw) % 360)
+        neg_source_crs13 = radians((crs13 - self.half_bw + 360) % 360)
+        pos_target_crs23 = radians((crs23 + self.half_bw) % 360)
+        neg_target_crs23 = radians((crs23 - self.half_bw + 360) % 360)
+
+        all_intersection = pd.Series()
+        #intersection 1
+        try:
+            pos_source_pos_target_x = self.intersection(lat2, lon2, pos_source_crs13, pos_target_crs23)
+        except ValueError as e:
+            all_intersection[0] = self.earth_circum
+        else:
+            all_intersection[0] = pos_source_pos_target_x[0]
+        #intersection 2
+        try:
+            pos_source_neg_target_x = self.intersection(lat2, lon2, pos_source_crs13, neg_target_crs23)
+        except ValueError as e:
+            all_intersection[1] = self.earth_circum
+        else:
+            all_intersection[1] = pos_source_neg_target_x[0]
+        #intersection 3
+        try:
+            neg_source_pos_target_x = self.intersection(lat2, lon2, neg_source_crs13, pos_target_crs23)
+        except ValueError as e:
+            all_intersection[2] = self.earth_circum
+        else:
+            all_intersection[2]= neg_source_pos_target_x[0]  
+        #intersection 4
+        try:
+            neg_source_neg_target_x = self.intersection(lat2, lon2, neg_source_crs13, neg_target_crs23)
+        except ValueError as e:
+            all_intersection[3] = self.earth_circum
+        else:
+            all_intersection[3] = neg_source_neg_target_x[0]
+
+        #return the closest neighbor
+        print(all_intersection)
+        return all_intersection.min()
+  
 # #test data
 # #lat lon in radians
-# lat2=radians(-1.2814722) #0.592539 -1.2814722 36.8209444
-# lon2=radians(36.8209444) #2.066470
+# lat1=radians(-1.2814722) #0.592539 -1.2814722 36.8209444
+# lon1=radians(36.8209444) #2.066470
 # #lat lon in radians
-# lat1=radians(-1.280823) #0.709186 -1.280823 36.822151
-# lon1=radians(36.822151) #1.287762
-# #azimuths to the intersection point
-# azim13 = radians(51)
-# azim23 =radians(300)
+# lat2=radians(-1.280823) #0.709186 -1.280823 36.822151
+# lon2=radians(36.822151) #1.287762
+# #confirm bearing and distance
+# source = Geodesic(lat1, lon1)
+# crs13 = radians(50)
+# crs23 = radians(270)
+# print("distance to point 2", source.distance(lat2, lon2))
+# try:
+#     print(source.bearing(lat2, lon2))
+#     print(source.intersection(lat2, lon2, crs13, crs23))
+# except ZeroDivisionError as error:
+#     print("same location")
 
-# #intersection example
-# #check the intersection function
-# source = geodesic(lat1, lon1,azim13)
-# dst12, dst13, dst23, lat3, lon3 = source.intersection(lat2, lon2, azim23)
-# print(dst12, dst13, dst23, lat3, lon3)
-# source2 = geodesic(lat2, lon2, azim23)
-# print('confirm distance dst23', source2.distance(radians(lat3), radians(lon3), radians(240)))
 
-test_azimuth = np.arange(0, 360, 30)
-ambigous_cases = 0
-infinity_cases = 0
-for a in test_azimuth:    
-    print(f'for azimuth {a}')
-    print(f'-----------------------')
-    a= radians(a)
-    try:
-        dst12, dst13, lat3, lon3 = source.intersection(lat2, lon2, a)
-        print(dst12, dst13, lat3, lon3)
-    except ValueError as e:
-         print("message from function:", str(e))
-         ambigous_cases +=1
-         infinity_cases +=1
-    
-print(f'total number = {len(test_azimuth)}, ambiguous case:{ambigous_cases}, infinity cases:{infinity_cases}')
+# test_azimuth = np.arange(0, 360, 30)
+# ambigous_cases = 0
+# infinity_cases = 0
+# for a in test_azimuth:    
+#     print(f'for azimuth {a}')
+#     print(f'-----------------------')
+#     a= radians(a)
+#     try:
+#         dst13, lat3, lon3 = source.intersection(lat2, lon2, crs13, a)
+#         print(dst13, lat3, lon3)
+#     except ValueError as e:
+#          print("message from function:", str(e))
+#          ambigous_cases +=1
+     
+# print(f'total number = {len(test_azimuth)}, ambiguous case:{ambigous_cases}, infinity cases:{infinity_cases}')
      
