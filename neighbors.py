06e6c05bda744ae89378745a9ac76c5588995082
fundi <fundigeorge@gmail.com>
2023-08-01 23:47:33 +0300

    testing for macro to micro IBS function

--- neighbors.py
+++ neighbors.py
@@ -1,5 +1,6 @@
-from math import radians
+from math import radians, degrees
 import pandas as pd
+import numpy as np
 from geodesic import Geodesic
 
 
@@ -22,22 +23,57 @@ if __name__ == "__main__":
     lat1=radians(-1.2814722) #0.592539 -1.2814722 36.8209444
     lon1=radians(36.8209444) #2.066470
     #lat lon in radians
-    lat2=radians(-1.280823) #0.709186 -1.280823 36.822151
+    lat2=radians(-1.280823) #0.709186 -1.280823 36.822151, 179, 239, 209(all ambigous)
     lon2=radians(36.822151) #1.287762
+    lat3=radians(-1.277733) # -1.277733  36.827901
+    lon3=radians(36.827901)
     crs13 = 20
-    crs23 = 270
+    crs23 = 212
 
     source = Geodesic(lat1, lon1)
-    closest_intersect = source.best_intersection(lat2, lon2, crs13, crs23)
-    print(closest_intersect)
-    crs13 = radians(50)
-    crs23 = radians(300)
-    print("manual calc", crs13, crs23)
+    bearing = source.bearing(lat2, lon2)
+    print(bearing)
     print("distance to point 2", source.distance(lat2, lon2))
+    crs13 = radians(bearing+10)
+    crs23 = radians(bearing+10)
+    fdelta = 5
+    closest_intersect = source.macro_best_intersection(lat2, lon2, crs13, crs23)
+    print('closest_intersect:fdelta=5', closest_intersect)
+    print("manual calc", degrees(crs13), degrees(crs23))
+    fdelta = 10 
+    closest_intersect = source.macro_best_intersection(lat2, lon2, crs13, crs23)  
+    print('closest_intersect:fdelta=10', closest_intersect) 
     try:
-        print(source.bearing(lat2, lon2))
+        
         print(source.intersection(lat2, lon2, crs13, crs23))
     except ZeroDivisionError as error:
         print("same location")
+    except ValueError as e:
+        print("ambigouos intersection")
 
-    
+#test for micro ibs
+    source_micro = Geodesic(lat1, lon1)
+    source_azim = radians(source_micro.bearing(lat2, lon2)+0)
+    crs12 = radians(source_micro.bearing(lat2, lon2))
+    print('testing macro to micro function\n...............')
+    ibs_directness = source_micro.micro_best_intersection(lat2, lon2, source_azim)
+   
+    print('directness', ibs_directness)
+
+
+# test_azimuth = np.arange(11, 360, 30)  
+# ambigous_cases = 0
+# crs13 = radians(20)
+# for a in test_azimuth:    
+#     print(f'for azimuth {a}')
+#     print(f'-----------------------')
+#     a = radians(a)
+#     try:
+#         neigh = source.best_intersection(lat2, lon2, crs13, a)
+#         print('best intersection', neigh)
+#     except ValueError as e:
+#          print("message from function:", str(e))
+#          ambigous_cases +=1
+     
+# print(f'total number = {len(test_azimuth)}, ambiguous case:{ambigous_cases}, ')
+     
