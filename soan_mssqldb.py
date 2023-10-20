05add48b863051fdbb588914882fa1ae6a88f0bf
fundigeorge <fundigeorge@gmail.com>
2023-10-20 12:31:36 +0300

    mssql server connection file using sqlalchemy or pyodbc

---
+++ soan_mssqldb.py
@@ -0,0 +1,18 @@
+from geodesic import Geodesic
+from neighborhood import Neighborhood
+from math import radians, degrees, pi
+import pandas as pd
+import pyodbc
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+
+#user select the site
+test_transmitter = "12001_NW_NI40010"
+#filter the transmitter from the db
+#record the transmitter lat an lon for filtering from the db the neighboring sites of the source site
+
+def main():
+    print("executing main")
+
+if __name__ == "__main__":
+    main()
