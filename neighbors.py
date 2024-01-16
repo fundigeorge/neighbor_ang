aa90440cfde497965f431b8965017af17cf58852
fundigeorge <fundigeorge@gmail.com>
2024-01-16 22:11:42 +0300

    finished get_cellname function that look up source and target transmitter params such as rnc, cellid, psc; added generate_intrafreq_script() function that create cmd for adding uintrafreq, started func for checking existing neighbors

--- neighbors.py
+++ neighbors.py
@@ -1,41 +1,44 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 
 def neighbor_ranking(s_neighs:pd.DataFrame, sitename, transmitter):
     #filter out the source site transmitters. captured by co_site relations
     #get transmitter with same sitename and whose distance from source < 50m. the 50m consider sector not at source but
-    #they are not far away to be considered ERRU and filter them out negation
-    print(s_neighs.head(20))
+    #they are not far away to be considered ERRU and filter them out negation    
     s_neighs = s_neighs.loc[~(s_neighs['site'].isin([sitename])&(s_neighs['dst12'] < 0.050))] #filter site displaced sector or ERRU
     s_neighs = s_neighs.loc[s_neighs['intersection'] != "source point"] #filter out site co-sector
     s_neighs['intersection'] = pd.to_numeric(s_neighs['intersection'])
-    print(s_neighs.head(20))
+
     #rank the neighbor by weighting intersection and dst12 variables. dst12 weight =0.7 and intersection = 0.3.
     #dst12 carries more weight because it shows a neighbor is near. transimmiter can be far and intersect closely
     s_neighs['neighborhood'] = s_neighs.loc[:, 'dst12'] * 0.7 + s_neighs.loc[:, 'intersection'] * 0.3 
+
     #ensure the neighborhood columns is numerical
     s_neighs['neighborhood'] = pd.to_numeric(s_neighs['neighborhood'])
-    print(s_neighs.head(20))
+    
     #set the top neighbor to select
     top_neighbor = st.number_input(label='Select number of top neighbor', min_value=10, max_value=32)
+
     #sort the neighbor based on neighborhood and pick top 20
     s_neighs = s_neighs.sort_values(by='neighborhood').nsmallest(n=top_neighbor, columns='neighborhood').reset_index()
+
     #add the source site and transmitter to the neighbor
     s_neighs['source_site'] = sitename
     s_neighs['source_transmitter'] = transmitter
-    print(s_neighs.head(20))
+    
     #modify the site->target_site and transmitter->target_transmitter
     s_neighs = s_neighs.rename(columns={'site':'target_site', 'transmitter':'target_transmitter'} )
-    print(s_neighs.head(20))
+    
     #re-arrange columns
     source_target_col = ['source_site', 'source_transmitter', 'target_site', 'target_transmitter' ]
     st.text_area(label='BEFORE ARRANGED COLUMNS')
     st.dataframe(s_neighs)
     s_neighs = s_neighs[source_target_col]
-    print(s_neighs.head(20))
-    s_neighs['rank'] = range(s_neighs.shape[0])
-    print(s_neighs.head(20))
+
+    #label the ranked neighbors
+    s_neighs['rank'] = range(s_neighs.shape[0])    
     
     return s_neighs
 
@@ -51,10 +54,63 @@ def bi_directional(neighbors:pd.DataFrame):
 
     return neighbors
 
+
+
 def get_cellname(neigh_rel:pd.DataFrame, db_conn):
     #define the query to select transmitter
-    query = "select * from transmitter_cellname where transmitter = %s"
-    transmitters = tuple(neigh_rel.loc[:,'source_transmitter'].drop_duplicates())
-    transmitter_cellname = pd.read_sql(query, con = db_conn, params=('12001_NW_NI20010'))
-    #join the neigh_rel table with cellname
-    print(transmitter_cellname)
+    #get the transmitter in the relations, target_transmitter has all transmitter due to defined bi-direction relations
+    transmitters = neigh_rel.loc[:, 'target_transmitter'].drop_duplicates()
+    transmitters = tuple(transmitters)
+    query = f""" select atoll_transmitter, bsc_name, logical_rnc_id, cell_name, cell_id, psc, lac, rac 
+              from umts_cells where atoll_transmitter IN {transmitters}"""
+    transmitters_params = pd.read_sql(query, con = db_conn, dtype={'logical_rnc_id':'int64', 'cell_id':'int64'})
+    #merge neigh_relation to get the source_transmitter and target_transmitter cell names and other details like, rncid
+    #get the source_transmitter details
+    print(transmitters_params.dtypes)
+    neigh_rel = pd.merge(neigh_rel, transmitters_params, how='left', 
+                         left_on='source_transmitter', right_on='atoll_transmitter',
+                         )
+    print(neigh_rel.columns, neigh_rel.dtypes)
+    #rename the source transmitter rnc, cellname, cellid as sources
+    neigh_rel = neigh_rel.rename(columns={
+                                            'bsc_name':'source_bscname',
+                                            'logical_rnc_id':'source_rncid',
+                                            'cell_id':'source_cellid',
+                                            'cell_name':'source_cellname',
+                                            })
+    print(neigh_rel.columns)
+    #for the source transmitter remove psc, rac, lac and merging column(atoll_transmitter) not needed for source Txs
+    neigh_rel = neigh_rel.drop(columns=['atoll_transmitter', 'psc', 'lac', 'rac'])
+    #get the target transmitter details
+    neigh_rel = pd.merge(neigh_rel, transmitters_params, how='left', 
+                         left_on='target_transmitter', right_on='atoll_transmitter')
+    print(neigh_rel.columns)
+    #rename the target transmitter rnc, cellname, cellid, psc .. as target
+    neigh_rel = neigh_rel.rename(columns =  {
+                                            'bsc_name':'target_bscname',
+                                            'logical_rnc_id':'target_rncid',
+                                            'cell_id':'target_cellid',
+                                            'cell_name':'target_cellname',
+                                            'psc':'target_psc',
+                                            'lac':'target_lac',
+                                            'rac':'target_rac'
+                                            })
+    #drop the megrging columns
+    neigh_rel = neigh_rel.drop(columns=['atoll_transmitter'])
+    #convert the rncid, cellid, psc, rac, lac float to integers
+    neigh_rel = neigh_rel.convert_dtypes()
+    return neigh_rel
+    print(neigh_rel.columns)
+    print(neigh_rel)
+
+def get_existing_neighbors(cellname:str, db_conn):
+    existing_neighbors_script = pd.DataFrame()
+    return existing_neighbors_script
+
+def generate_intrafreq_script(neigh_rel:pd.DataFrame):
+    #create the mml command for creating new neighbors
+    #iterate through the rows
+    for row in neigh_rel.itertuples(index=False):
+        neigh_rel['uintrafreq_cmd'] = "ADD UINTRAFREQ RNC =" + str(row.source_rncid) + "SCELLID=" + str(row.source_cellid)
+    
+    return neigh_rel
